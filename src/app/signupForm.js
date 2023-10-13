4833f38763e9c32e86c8bff6fd9432ef737dc631
david pinto <dbpintogonza@gmail.com>
2023-10-13 01:17:05 -0500

    09

---
+++ src/app/signupForm.js
@@ -0,0 +1,37 @@
+import {createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js"
+import { auth } from "./firebase.js"
+
+const signupForm = document.querySelector('#signup-form')
+
+signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    const email = signupForm['signup-email'].value
+    const password =signupForm['signup-password'].value
+
+    console.log(email,password)
+
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        console.log(userCredentials)
+
+        const signupModal = document.querySelector('#signupModal')
+        const modal = bootstrap.Modal.getInstance(signupModal)
+        modal.hide()
+    } catch (error) {
+        console.log(error.message)
+        console.log(error.code)
+        if (error.code === 'auth/email-already-in-use'){
+            alert ('Email already in use')
+        } else if (error.code === 'auth/invalid-email') {
+            alert('Invalid email')
+        } else if (error.code === 'auth/weak-password') {
+            alert ('password is too weak')
+        } else if (error.code) {
+            alert ('Something went wrong')
+        }
+
+    }
+
+
+})
